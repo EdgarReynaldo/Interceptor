e6d1d3d734ceefced463670a6e49c4d053831ce7
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2018-11-06 14:54:37 -0600

    Fixed minus

--- Vector2.hpp
+++ Vector2.hpp
@@ -34,7 +34,7 @@ inline Vec2 operator+(const Vec2& v1 , const Vec2& v2) {
    return Vec2(v1.x + v2.x , v1.y + v2.y);
 }
 inline Vec2 operator-(const Vec2& v1 , const Vec2& v2) {
-   return Vec2(v1.x + v2.x , v1.y + v2.y);
+   return Vec2(v1.x - v2.x , v1.y - v2.y);
 }
 
 
